38d80b7ab9f009b0e9925fced40b3f01087199ef
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2024-02-13 11:58:19 +1300

    Lunchtime commit as requested by supervisor. Complete to The Guessing Game.

---
+++ index.js
@@ -0,0 +1,24 @@
+let favoriteFood = "pizza";
+console.log( favoriteFood );
+
+let age = 22;
+if ( age >= 18 )
+{
+  console.log( "Is an adult" );
+} else
+{
+  console.log( "Is not an adult" );
+}
+
+let secretNumber = "33";
+let guess = prompt( "Guess the Secret NUMBER" );
+if ( guess == "33" )
+{
+  console.log( "You Got It!!" );
+} else
+{
+  console.log( "Guess Again!!" );
+}
+
+
+
