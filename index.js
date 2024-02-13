ec6d139e5fcbf2da55ea9bae9e20acd557c7e261
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2024-02-13 13:03:25 +1300

    Complete to Simple Math Test

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ if ( age >= 18 )
   console.log( "Is not an adult" );
 }
 
-let secretNumber = "33";
-let guess = prompt( "Guess the Secret NUMBER" );
-if ( guess == "33" )
-{
-  console.log( "You Got It!!" );
-} else
-{
-  console.log( "Guess Again!!" );
-}
+// let secretNumber = "33";
+// let guess = prompt( "Guess the Secret NUMBER" );
+// if ( guess == "33" )
+// {
+//   console.log( "You Got It!!" );
+// } else
+// {
+//   console.log( "Guess Again!!" );
+// }
 
+let userNumOne = prompt( "Please provide a NUMBER" );
+let userNumTwo = prompt( "Please provide another NUMBER" );
 
-
+console.log( Number( userNumOne ) + Number( userNumTwo ) )
