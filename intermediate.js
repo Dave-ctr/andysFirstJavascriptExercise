881f8c52d30e80102ce390be3ae7f147c3bd6301
Dave-ctr <83482840+Dave-ctr@users.noreply.github.com>
2024-02-14 12:33:05 +1300

    Lunchtime. Complete to Temperature Converter.

---
+++ intermediate.js
@@ -0,0 +1,36 @@
+// || Budget Planner
+// let monthlyIncome = prompt( "Please enter your MONTHLY INCOME" );
+// let monthlyExpenses = prompt( "Please enter your MONTHLY EXPENSES", "Sum of rent, utilities and food" );
+// let netIncome = monthlyIncome - monthlyExpenses;
+// if ( netIncome >= 0 )
+// {
+//   console.log( "You are within your budget WHOOP WHOOP!!" );
+// } else
+// {
+//   console.log( "You are over your budget!! Waahhh!!" );
+// }
+
+// || Event Scheduler
+// let eventDay = prompt( "What is the day of your event?", 'e.g., "Monday"' );
+// let availability = prompt( "Are you available on that day?", "Yes or No" );
+// if ( availability == "Yes" )
+// {
+//   console.log( "Users attendance has been confirmed." );
+// } else
+// {
+//   console.log( "User expresses their regret that they cannot attend." );
+// }
+
+// || Temperature Converter
+// let userTemperature = prompt( "Enter your temperature in Celsius or Fahrenheit", 'e.g., "22C or 22F"' );
+// if ( userTemperature.endsWith( "C" ) )
+// {
+//   let convertedTemperature = ( parseInt( userTemperature ) * 9 / 5 + 32 );
+//   let newTemperature = convertedTemperature + "F";
+//   console.log( newTemperature );
+// } else if ( userTemperature.endsWith( "F" ) )
+// {
+//   let convertedTemperature = ( ( parseInt( userTemperature ) - 32 ) / ( 9 / 5 ) );
+//   let newTemperature = convertedTemperature + "C";
+//   console.log( newTemperature );
+// }
